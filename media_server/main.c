52c9155fdbbfd104db88518b913689e3579e879b
pinjiang <pin.jiang@outlook.com>
2019-09-10 08:34:37 +0800

    修正media_server/main.c中的Windows编译宏的错误

--- media_server/main.c
+++ media_server/main.c
@@ -63,7 +63,7 @@ int main(int argc, char *argv[]) {
 	/* 2.开始启动服务 */
 	glib_log_info("Media Server Started");
 	signal (SIGINT, quit);
-#if not WIN32
+#ifdef WIN32
 	signal(SIGKILL, quit);
 #endif
 	signal (SIGTERM, quit);
